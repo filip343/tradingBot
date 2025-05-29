add validation for transformer models fix some bugs connected to logging during training fix the target calculation move some logic to other functions

app.py
@@ -8,6 +8,9 @@ import torch
 import numpy as np
 import pandas as pd
 from dataset import Dataset
+from sklearn.model_selection import GroupShuffleSplit
+from sklearn.preprocessing import StandardScaler
+
 class App():
     def __init__(self):
         self.config = {
@@ -18,7 +21,7 @@ class App():
             "num_workers":4,
             "lr":1e-3,
             "max_epochs":4,
-            "time_size":10,
+            "time_size":64,
         }
         
     def add_features(self,data,features=[]):
@@ -50,40 +53,70 @@ class App():
 
         for symbol in symbols:
             loaded = load_func(symbol,intervals=interval,period="max")
-            if loaded is not None and not loaded.empty:
-                symbol_col = pd.Series(np.ones(len(loaded))*symbols.index(symbol),name="symbol")
-                loaded = pd.concat([loaded.reset_index(), symbol_col], axis=1)
-                data.append(loaded)
+            res = self.preprocess_loaded_data(loaded,symbols.index(symbol),features)
+            if res is not None:
+                data.append(res)
                 num_loaded_symbols+=1
-        for i,df in enumerate(data):
-            df = self.add_features(df,features)
-            data[i] = df
         data = pd.concat(data)
         data["id"] = np.arange(len(data))
         columns = list(data.columns)
         columns.remove("symbol")
         columns.remove("id")
         data = data.reindex(columns=["id","symbol"]+columns)
-        data.drop(columns=["Date","open"],inplace=True)
         print(f"Done in {time.time()-now}s")
         print(f"Loaded: {num_loaded_symbols} symbols")
         return (data,symbols)
+    def preprocess_loaded_data(self,loaded,symbol_id,features):
+        if loaded is None or loaded.empty:
+            return None
+        scaler = StandardScaler()
+        symbol_col = pd.Series(np.ones(len(loaded))*symbol_id,name="symbol",dtype=np.float32)
+        loaded = pd.concat([loaded.reset_index(), symbol_col], axis=1)     
+        loaded = self.add_features(loaded,features)
+        loaded = loaded.drop(columns=["index","Date","open","splits","dividends"])
+        loaded = loaded.dropna()
+        loaded = loaded.astype(np.float32)
+        feature_columns = loaded.columns
+        feature_columns = feature_columns.drop("symbol")
+        if "target" in loaded.columns:
+            feature_columns = feature_columns.drop("target")
+        loaded.loc[:,feature_columns] = scaler.fit_transform(loaded.loc[:,feature_columns])
+        return loaded 
 
-    def get_data_loader(self,data,labels,dataset_type=None):
+    def get_data_loaders(self,data,labels,train_split=0.8,dataset_type=None):
         data = torch.tensor(data,dtype=torch.float32)
         labels = torch.tensor(labels,dtype=torch.float32)
+        groups = data[:,1].numpy()
+        gss = GroupShuffleSplit(n_splits=1,train_size=train_split)
+        gss.get_n_splits()
+        (train_index,val_index) = [split for split in gss.split(data,labels,groups)][0]
+        X_train = data[train_index]
+        y_train=labels[train_index]
+        X_val = data[val_index]
+        y_val=labels[val_index]
+        print(X_train.shape)
+        print(X_val.shape)
         if dataset_type=="single_time" :
-            dataset = torch.utils.data.TensorDataset(data,labels)
+            dataset_train = torch.utils.data.TensorDataset(X_train,y_train)
+            dataset_val = torch.utils.data.TensorDataset(X_val,X_val)
         else:
-            dataset = Dataset(data,labels,time_size=self.config["time_size"])
-        data_loader = torch.utils.data.DataLoader(
-            dataset=dataset,
+            dataset_train = Dataset(X_train,y_train,time_size=self.config["time_size"])
+            dataset_val = Dataset(X_val,y_val,time_size=self.config["time_size"])
+        train_data_loader = torch.utils.data.DataLoader(
+            dataset=dataset_train,
             batch_size=self.config["batch_size"],
             shuffle=True,
             persistent_workers=True,
             num_workers=self.config["num_workers"]
         )
-        return data_loader
+        val_data_loader = torch.utils.data.DataLoader(
+            dataset=dataset_val,
+            batch_size=self.config["batch_size"],
+            shuffle=False,
+            persistent_workers=True,
+            num_workers=self.config["num_workers"]
+        )
+        return train_data_loader,val_data_loader
     def load_config(self,config_path:str)->None:
         config_path = Path(config_path)
         if config_path.suffix !=".json":

dataset.py
@@ -15,11 +15,11 @@ class Dataset(torch.utils.data.Dataset):
             first = torch.min(ids).item()
             last = torch.max(ids).item()-self.time_size+1
             if last<first:continue
-            idx.extend(list(range(first,last))) 
+            idx.extend(list(range(first,last)))
 
         self.idx = idx
     def __getitem__(self, index):
         index = self.idx[index]
-        return (self.X[index:index+self.time_size],self.Y[index:index+self.time_size])
+        return (self.X[index:index+self.time_size,2:],self.Y[index:index+self.time_size])
     def __len__(self):
         return len(self.idx)

feature_calcs.py
@@ -103,11 +103,11 @@ class FeaturesCalc():
         if any(feat not in data.columns for feat in lag_features):
             raise ValueError(f"columns: {lag_features} are not in data")
         for feat in lag_features:
-            data[feat+"_lag"] = data[feat].shift(lag_time)
-        return data[[feat+"_lag" for feat in lag_features]]
+            data[feat+f"_lag_{lag_time}"] = data[feat].shift(lag_time)
+        return data[[feat+f"_lag_{lag_time}" for feat in lag_features]]
     def create_labels(self,column="close"):
         data =self.data
-        if column+"_lag" not in data.columns:
-            self.create_lags([column],lag_time=5)
-        data["target"] = np.where(data[column+"_lag"]>data[column],0,1)# 0- less , 1- greater
+        self.create_lags([column],lag_time=-1)
+        data["target"] = np.where(data[column+f"_lag_{-1}"]>data[column],1,0)# 0- less , 1- greater
+        del data[column+f"_lag_{-1}"]
         return data["target"]

models.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import lightning as L
 import lightgbm as lgb
+from pytorch_lightning.loggers import CSVLogger
+import pandas as pd
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class ModelHandler():
@@ -39,14 +41,17 @@ class TorchModelWrapper():
     def __init__(self,predictor:nn.Module,lr=1e-3,max_epochs=4):
         self.predictor = predictor
         self.model = Model(self.predictor,lr)
+        logger = CSVLogger("./",name="lightning_logs")
         self.trainer = L.Trainer(
+            logger=logger,
             min_epochs=1,
             max_epochs=max_epochs,
             devices="auto",
             accelerator=DEVICE.type,
             log_every_n_steps=100,
             #deterministic=True,
-            benchmark=True
+            benchmark=True,
+            val_check_interval=0.05
             )
     def fit(self,data_loader,val_loader=None):
         self.trainer.fit(self.model,data_loader,val_loader)
@@ -62,8 +67,21 @@ class Model(L.LightningModule):
     def training_step(self,batch,batch_idx):
         x,y = batch
         y_hat = self.predictor(x)
-        loss = F.mse_loss(y_hat,y) 
+        loss = F.binary_cross_entropy_with_logits(y_hat.flatten(),y.flatten().float()) 
         self.log("train_loss",loss)
+        return loss
+    def validation_step(self,batch,batch_idx):
+        with torch.no_grad():
+            x,y=batch
+            logits = self.predictor(x)
+            probs = torch.sigmoid(logits)
+            preds = (probs>0.5).float().squeeze(-1)
+            loss = F.binary_cross_entropy_with_logits(logits,y.float())
+            acc = (preds.flatten()==y.flatten().float()).float().mean()
+
+            self.log("val_loss",loss,prog_bar=True,on_epoch=True)
+            self.log("val_acc",acc,prog_bar=True,on_epoch=True)
+
         return loss
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(),self.lr)
@@ -79,7 +97,7 @@ class Transformer(nn.Module):
             dim_feedforward=2048,
             dropout=0.1,
             activation=F.gelu,
-            batch_first=True
+            batch_first=True,
         )
         self.encoder = nn.TransformerEncoder(
             encoder_layer=self.enc_layer,
@@ -87,7 +105,9 @@ class Transformer(nn.Module):
         )
         self.lin2 = nn.Linear(hidden_size,output_size)
     def forward(self,x):
+        time_size = x.shape[1]
+        mask = torch.triu(torch.ones(time_size,time_size), diagonal=1).bool()
         x = F.gelu(self.lin1(x))
-        x = self.enc_layer(x)
-        x = F.sigmoid(self.lin2(x))
+        x = self.enc_layer(x,mask)
+        x=self.lin2(x)
         return x